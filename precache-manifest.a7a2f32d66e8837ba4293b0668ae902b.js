12f17e4fd6ad537c9482b09c4cbf3609c80e7b80
Radmir <batradmir@gmail.com>
2022-01-14 13:39:04 +0500

    deploy

---
+++ precache-manifest.a7a2f32d66e8837ba4293b0668ae902b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e557fb8dbad89e1f345",
+    "url": "/testwork/css/app.399dfa4d.css"
+  },
+  {
+    "revision": "19ba8c840ca0dfd07ee0",
+    "url": "/testwork/css/chunk-vendors.ac5b10c9.css"
+  },
+  {
+    "revision": "6fd8bb8a55ff456aeab680f272664e28",
+    "url": "/testwork/index.html"
+  },
+  {
+    "revision": "4e557fb8dbad89e1f345",
+    "url": "/testwork/js/app.073b6354.js"
+  },
+  {
+    "revision": "19ba8c840ca0dfd07ee0",
+    "url": "/testwork/js/chunk-vendors.822ddf78.js"
+  },
+  {
+    "revision": "7a2087cbf6e11538ea34ee0b4563d8db",
+    "url": "/testwork/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/testwork/robots.txt"
+  }
+]);
